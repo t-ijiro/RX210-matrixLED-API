24f6142ddc63659818e82e9c74f6262966f05dd7
t-ijiro <tamitoijiro1125@gmail.com>
2026-01-08 05:37:14 +0900

    pwm_loop.c を更新

--- pwm_loop.c
+++ pwm_loop.c
@@ -1,6 +1,6 @@
 void loop()
 {
-    if(t == shrehold)
+    if(t == threhold)
     {
         matrix_write(0, 0, pwm_mode ? pixel_off : c);
         matrix_flush();
@@ -9,12 +9,12 @@ void loop()
     if(t == 0)
     {
         t = T_MAX;
-        shrehold++;
+        threhold++;
 
-        if(shrehold == T_MAX)
+        if(threhold == T_MAX)
         {
             pwm_mode ^= 1;
-            shrehold = 0;
+            threhold = 0;
         }
         
         matrix_write(0, 0, pwm_mode ? c : pixel_off);
