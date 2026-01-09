a5e758bd75b61decdfa447ea68087c83a2aa234a
t-ijiro <tamitoijiro1125@gmail.com>
2026-01-10 03:02:13 +0900

    matrix_config.h を更新

--- matrixAPI/src/matrix_config.h
+++ matrixAPI/src/matrix_config.h
@@ -1,8 +1,8 @@
 #ifndef MATRIX_CONFIG_H
 #define MATRIX_CONFIG_H
 
-// ISR内でmatrixAPIを使用するかどうかの設定
-#define MATRIX_USE_IN_ISR 0
+// ISR内でダイナミック点灯処理をするかどうかの設定
+#define MATRIX_DYNAMIC_IN_ISR 1
 
 // フォント機能を使用するかどうかの設定
 #define MATRIX_USE_FONT 1
